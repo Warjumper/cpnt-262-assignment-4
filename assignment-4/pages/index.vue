1f79d042a410df14d263a640c31ebea967a18417
unknown <jessehorner11@gmail.com>
2022-11-28 16:02:48 -0700

    project thus far, added talwind

---
+++ assignment-4/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi praesentium vero iusto maxime laborum quam explicabo dignissimos perferendis repellat ad?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi praesentium vero iusto maxime laborum quam explicabo dignissimos perferendis repellat ad?</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+
+  p {
+    margin: 20px;
+  }
+</style>
