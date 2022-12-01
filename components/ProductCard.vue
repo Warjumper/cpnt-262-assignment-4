f15ebed8e2bf769ac718a9b00f90ef28def014a7
unknown <jessehorner11@gmail.com>
2022-11-30 20:25:39 -0700

    spelling correction

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -3,7 +3,7 @@
     <img :src="product.image" alt="product thum" class="thumb">
     <p class="font-bold text-gray-500 m-4 truncate">{{ product.title }}</p>
     <NuxtLink :to="`/products/${product.id}`">
-      <p class="btn my-4">View Deatails</p>
+      <p class="btn my-4">View Details</p>
     </NuxtLink>
   </div>
 </template>
